f3d003b4c4b165a7a3019703ecb6d2145a1ef98c
ph-song <songpinhen@gmail.com>
2023-09-19 14:42:17 +1000

    frontend

--- frontend/src/Forecasting.js
+++ frontend/src/Forecasting.js
@@ -1,6 +1,5 @@
 import { Button, FileInput, H1, H2} from "@blueprintjs/core";
 import { useState, useEffect} from "react";
-import { UserData } from "./data.js";
 import './App.css';
 import { Line } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
@@ -8,40 +7,85 @@ import axios from "axios";
 
 function Forecasting() {
 
-  const [data, setData] = useState()
   const [file, setFile] = useState()
+  const [fileName, setFileName] = useState()
 
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.Time),
-    datasets: [
-      {
-        label: "Electricity demand",
-        data: UserData.map((data) => data.Load/1000)
-    }
-    ],
-  });
+
+  const [chartData, setChartData] = useState({datasets: [{data: [],},],});
+  const [options, setOptions]  = useState({scales: {x: {},y: {},}});
+  
+  useEffect( () => {
+
+    axios({
+      url:'http://localhost:888/',
+      method: 'get'
+    })
+    .then((response) =>{
+
+      console.log(response.data)
+      const labels = response.data.actual.map((item) => item.time);
+      const values1 = response.data.actual.map((item) => item.load_kw/1000);
+    
+
+      // Create a Chart.js-compatible data structure
+      const chartData = {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Electricity Demand',
+            data: values1
+          }
+        ],
+      };
+      
+      const options = {
+        scales: {
+          x: {
+            title: {
+              display: true,
+              text: 'Time', // Customize the y-axis label here
+            }
+          },
+          y: {
+            title: {
+              display: true,
+              text: 'Load (MW)', // Customize the y-axis label here
+            },
+          },
+        },
+      };
+      
+      setOptions(options);
+      setChartData(chartData);  //*/
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+    }, [])
+
 
   const handleFile = (e)=>{
-    if (e.target.files[0].type !== 'text/csv'){
+    const file = e.target.files[0]
+    if (file.type !== 'application/zip'){
         alert("wrong file format")
+        setFileName()
         return
     }else{
-        setFile(e.target.files[0]);
-        console.log(123)
+      setFileName(file.name)
+      setFile(file);
     }
   }
 
   const handleUpload = (e)=>{
     const data = new FormData()
-    data.append('uploadcsv', file)//req.file
-
+    data.append('zip_file', file)//req.fil
     axios({
       url:"http://localhost:888/upload",
       method:"post",
       data: data
     })
     .then(function(res){
-      alert(res.data.msg)
+      alert(res)
       window.location.reload(false)
     })
     .catch(function(err){
@@ -49,23 +93,6 @@ function Forecasting() {
       e.preventDefault()
     })
   }
-
-
-  useEffect(() =>{
-    axios({
-      url:'http://localhost:888/',
-      method: 'get'
-    })
-    .then(function(res){
-      if (res.status === 200){
-        setData(res.data)
-      }
-    })
-    .catch(function(err){
-      console.log('error')
-    })
-  })
-
 
 
     return (
@@ -76,14 +103,14 @@ function Forecasting() {
           <H1 className="">Electricity Demand</H1>
 
           <div className="" style={{position: "relative", height:"45vh", width:"90vw"}}>
-            <Line data={userData} />
+            <Line data={chartData} options={options}/>
           </div>
 
           <H2 className=''>Data Upload</H2>
 
           <div className='row'>
 
-            <FileInput className='col-xs-10' fill={false}  onInputChange={handleFile} large={true}/>
+            <FileInput className='col-xs-10' fill={false} text={fileName} onInputChange={handleFile} large={true}/>
             
             <Button className='col-xs-2' onClick={handleUpload} large={true}> upload </Button>
           </div>
