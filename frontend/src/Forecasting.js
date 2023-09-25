2860d51c6a25c260b2870d73ee2125861bae26a0
ph-song <songpinhen@gmail.com>
2023-09-25 15:19:11 +1000

    niave

--- frontend/src/Forecasting.js
+++ frontend/src/Forecasting.js
@@ -1,4 +1,4 @@
-import { Button, FileInput, H1, H2} from "@blueprintjs/core";
+import { Button, ButtonGroup, FileInput, H1, H2, H3} from "@blueprintjs/core";
 import { useState, useEffect} from "react";
 import './App.css';
 
@@ -8,11 +8,13 @@ import { Line } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import 'chartjs-adapter-date-fns';
 
+import { ColorRing } from  'react-loader-spinner'
 
 function Forecasting() {
-
   const [file, setFile] = useState(null)
   const [fileName, setFileName] = useState()
+
+  const [isLoading, setIsLoading] = useState(false)
 
   const [chartData1, setChartData1] = useState({datasets: [{data: [],},],});
   const [options1, setOptions1]  = useState({scales: {x: {},y: {},}});
@@ -20,8 +22,8 @@ function Forecasting() {
   const [chartData2, setChartData2] = useState({datasets: [{data: [],},],});
   const [options2, setOptions2]  = useState({scales: {x: {},y: {},}});
   
+  //whenever page is loaded
   useEffect( () => {
-
     axios({
       url:'http://localhost:888/',
       method: 'get'
@@ -30,32 +32,43 @@ function Forecasting() {
       console.log(response.data)
 
       //model prediction
-      const labels1 = response.data.actual.map((item) => new Date(item.time));
-      const values1 = response.data.actual.map((item) => item.load_kw/1000);
-      const result1 = labels1.map((value, index) => ({ x: value, y: values1[index] }));
+      const labels = response.data.actual.map((item) => new Date(item.time));
+      const values = response.data.actual.map((item) => item.load_kw/1000);
+      const result = labels.map((value, index) => ({ x: value, y: values[index] }));
       
-      const result2 = parseData(response.data.predict, 'xgb_load')
-      const result3 = parseData(response.data.predict, 'lgb_load')
+      const resultPredXGB = parseData(response.data.predict, 'xgb_load')
+      const resultPredLGB = parseData(response.data.predict, 'lgb_load')
+      const resultPredNaive48 = parseData(response.data.predict, 'n48_load')
+      const resultPredNaive168 = parseData(response.data.predict, 'n168_load')
 
       const chartData1 = {
-        labels: labels1,
+        labels: labels,
         datasets: [
           {
             label: 'Electricity Demand',
-            data: result1
+            data: result
           },
           {
             label: 'XGBoost',
-            data: result2
+            data: resultPredXGB
           },
           {
             label: 'LightGBM',
-            data: result3
+            data: resultPredLGB
+          },
+          {
+            label: 'Naive',
+            data: resultPredNaive48
+          },
+          {
+            label: 'Seasonal Naive',
+            data: resultPredNaive168
           }
         ],
       };
       
       const options1 = {
+        
         scales: {
           x: {
             type: 'time', time:{unit: 'day'},
@@ -78,19 +91,29 @@ function Forecasting() {
       setChartData1(chartData1);  //*/
 
       //model error
-      const result4 = parseData(response.data.predict, 'xgb_error')
-      const result5 = parseData(response.data.predict, 'lgb_error')
+      const resultErrXGB = parseData(response.data.predict, 'xgb_error')
+      const resultErrLGB = parseData(response.data.predict, 'lgb_error')
+      const resultErrNaive48 = parseData(response.data.predict, 'n48_error')
+      const resultErrNaive168 = parseData(response.data.predict, 'n168_error')
 
       const chartData2 = {
-        labels: labels1,
+        labels: labels,
         datasets: [
           {
             label: 'XGBoost',
-            data: result4
+            data: resultErrXGB
           },
           {
             label: 'LightGBM',
-            data: result5
+            data: resultErrLGB
+          },
+          {
+            label: 'Naive',
+            data: resultErrNaive48
+          },
+          {
+            label: 'Seasonal Naive',
+            data: resultErrNaive168
           }
         ],
       };
@@ -123,6 +146,7 @@ function Forecasting() {
       });
     }, [])
 
+
   function parseData(data, key){
     const labels = data.filter(item => key in item && item[key] > 0).map(item => new Date(item['time']))
     const values = data.filter(item => key in item && item[key] > 0).map(item=>item[key])
@@ -130,7 +154,9 @@ function Forecasting() {
     return result
   }
 
+  //when a file is uploaded 
   const handleFile = (e)=>{
+
     const file = e.target.files[0]
     if (file.type !== 'application/zip'){
         alert("wrong file format")
@@ -142,7 +168,9 @@ function Forecasting() {
     }
   }
 
+  //when upload button is pressed
   const handleUpload = (e)=>{
+    setIsLoading(true)
     if (file === null){
       alert("please upload file")
       return 
@@ -156,9 +184,11 @@ function Forecasting() {
     })
     .then(function(res){
       alert('file uploaded')
+      setIsLoading(false)
       window.location.reload(false)
     })
     .catch(function(err){
+      setIsLoading(false)
       alert(err.response.data.msg)
       e.preventDefault()
     })
@@ -167,32 +197,40 @@ function Forecasting() {
 
     return (
       <div className="container">
+        
+        <H1>Electricity Demand</H1>
 
-        <div className= ''>
-
-          <H2 className="">Electricity Demand</H2>
-
-          <div className="" style={{position: "relative", height:"45vh", width:"90vw"}}>
-            <Line data={chartData1} options={options1}/>
-          </div>
-
-          <H2 className="">Model Error</H2>
-
-          <div className="" style={{position: "relative", height:"45vh", width:"90vw"}}>
-            <Line data={chartData2} options={options2}/>
-          </div>
-
-          <H2 className=''>Data Upload</H2>
-
-          <div className='row'>
-
-            <FileInput className='col-xs-10' fill={false} text={fileName} onInputChange={handleFile} large={true}/>
-            
-            <Button className='col-xs-2' onClick={handleUpload} large={true}> upload </Button>
-          </div>
-          
+        <div className='row' style={{position: "relative",height:"45vh",  width:"90vw"}}>
+          <H3>Forecast</H3>
+          <Line data={chartData1} options={options1}/>
         </div>
 
+        <div className='row'>
+          <H3>Data Upload</H3>
+          <FileInput className='col-xs-10 col-md-6' fill={false} text={fileName} onInputChange={handleFile} large={true}/>
+          <Button className='col-xs-2' onClick={handleUpload} disabled= {isLoading} large={true}> upload </Button>
+        </div>
+
+        <div className='row' style={{position: "relative", height:"45vh", width:"90vw"}}>
+          <H3>Model Error</H3>
+          <Line data={chartData2} options={options2}/>
+        </div>
+
+        <div className='row' 
+          style={{position: 'absolute',top: '50%',left: '50%',
+          transform: 'translate(-50%, -50%)',zIndex: 9999}}
+          >
+          <ColorRing
+            visible={isLoading}
+            height="150"
+            width="150"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          />
+        </div>
+          
       </div>
     );
   }
